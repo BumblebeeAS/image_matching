aa62ec2e0fcc1335f32068ef7f5ed8631b45e2b0
Xing Yu Ng <ngxingyu@comp.nus.edu.sg>
2023-01-04 17:57:05 +0800

    Fixes implementation of keypoints to subclass np.ndarray instead of namedtuple

--- src/feature_matcher/keypoints.py
+++ src/feature_matcher/keypoints.py
@@ -1,16 +1,30 @@
-from typing import NamedTuple, Tuple
+from typing import Tuple
 import numpy as np
 
-class Keypoints(NamedTuple):
-    image_size: Tuple[int, int] # width, height
-    keypoints: np.ndarray
-    descriptors: np.ndarray
-    scores: np.ndarray
+class Keypoints(np.ndarray):
 
-    def __getitem__(self, key):
-        return Keypoints(self.image_size,
-                         self.keypoints[key],
-                         None if self.descriptors is None else self.descriptors[key],
-                         None if self.descriptors is None else self.scores[key])
-    def __len__(self):
-        return len(self.keypoints)
+    def __new__(cls, image_size: Tuple[int, int], keypoints: np.ndarray, descriptors: np.ndarray = None, scores: np.ndarray = None):
+        '''
+        params:
+            image_size: (width, height)
+            keypoints: np.ndarray (x, y) * N
+            descriptors: np.ndarray (descriptor_size) * N
+            scores: np.ndarray (score) * N 
+        Represented as a single numpy array with the following columns:
+        keypoints (x, y): 0-1, scores: 2, descriptors: rest
+        '''
+        obj = np.hstack([keypoints, scores.squeeze()[:, None], descriptors]).astype(np.float32).view(cls)
+        obj.image_size = image_size
+        return obj
+    def __array_finalize__(self, obj):
+        if obj is None: return
+        self.image_size = getattr(obj, 'image_size', None)
+    @property
+    def descriptors(self):
+        return self[:, 3:]
+    @property
+    def keypoints(self):
+        return self[:, :2]
+    @property
+    def scores(self):
+        return self[:, 2]
