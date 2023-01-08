e0259a2522911081c76fe0af21b30ee02b8a9b7c
Amadeus <amadeusw6@gmail.com>
2023-01-08 12:08:58 +0800

    Fix keypoints for detector-free matchers

--- src/feature_matcher/keypoints.py
+++ src/feature_matcher/keypoints.py
@@ -1,9 +1,9 @@
-from typing import Tuple
+from typing import Optional, Tuple
 import numpy as np
 
 class Keypoints(np.ndarray):
 
-    def __new__(cls, image_size: Tuple[int, int], keypoints: np.ndarray, descriptors: np.ndarray = None, scores: np.ndarray = None):
+    def __new__(cls, image_size: Tuple[int, int], keypoints: np.ndarray, descriptors: Optional[np.ndarray] = None, scores: np.ndarray = None):
         '''
         params:
             image_size: (width, height)
@@ -13,14 +13,22 @@ class Keypoints(np.ndarray):
         Represented as a single numpy array with the following columns:
         keypoints (x, y): 0-1, scores: 2, descriptors: rest
         '''
-        obj = np.hstack([keypoints, scores.squeeze()[:, None], descriptors]).astype(np.float32).view(cls)
+        to_be_stacked = [keypoints, scores.squeeze()[:, None]]
+        if descriptors is not None:
+            to_be_stacked.append(descriptors)
+
+        obj = np.hstack(to_be_stacked).astype(np.float32).view(cls)
         obj.image_size = image_size
+        obj.has_descriptors = descriptors is not None
         return obj
     def __array_finalize__(self, obj):
         if obj is None: return
         self.image_size = getattr(obj, 'image_size', None)
+        self.has_descriptors = getattr(obj, "has_descriptors", None)
     @property
     def descriptors(self):
+        if not self.has_descriptors:
+            return None
         return self[:, 3:]
     @property
     def keypoints(self):
