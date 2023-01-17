b5c210971df5707e4e903f22fc41232b93788c2a
1-karthigeyan-1 <karthigeyan98@yahoo.com.sg>
2023-01-17 23:15:19 +0800

    fix input to persptrsfm

--- scripts/annotation.py
+++ scripts/annotation.py
@@ -18,10 +18,10 @@ class annotator(BasicFeatureMatcher):
         max_pt = np.amax(pts, axis=0)
         min_pt = np.amin(pts, axis=0)
         #Limit checking
-        if (max_pt > (h,w)):
-                max_pt = np.float32([h,w])
-        if (min_pt < (0,0)):
-            min_pt = np.float32([0,0])
+        #if (max_pt > (h,w)):
+        #        max_pt = np.float32([h,w])
+        #if (min_pt < (0,0)):
+        #    min_pt = np.float32([0,0])
         return np.float32([max_pt[0], max_pt[1], max_pt[0], min_pt[1], min_pt[0], min_pt[1], min_pt[0], max_pt[1]]).reshape(4,1,2)
 
     def cropped_image_callback(self, img_msg, detected_objects=None, debug=False):
@@ -46,32 +46,35 @@ class annotator(BasicFeatureMatcher):
 
 
         kp1, kp2 = self.image_match_producer.process_image(img, self.template, lxtyrxby = lxtyrxby,  debug=True) #kp1 -> template, kp2 -> img
-        src_pts = np.float32([kp1[i].keypoints for i in range(len(kp1))]).reshape(-1,1,2) 
-        dst_pts = np.float32([kp2[i].keypoints for i in range(len(kp2))]).reshape(-1,1,2)
+        src_pts = np.float32([kp1.keypoints]).reshape(-1,1,2) 
+        dst_pts = np.float32([kp2.keypoints]).reshape(-1,1,2)
+
         M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-
         h,w = self.template_img.shape[:2]
+
         pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-        dst = cv2.perspectiveTransform(pts,M)
-        dst = self.convertToRect(dst, h, w)
+        
+        if(M is not None):
+            dst = cv2.perspectiveTransform(pts,M)
+            dst = self.convertToRect(dst, h, w)
 
-        img2 = cv2.polylines(img, [np.int32(dst)], True, (0,255,0),3, cv2.LINE_AA)
+            img2 = cv2.polylines(img, [np.int32(dst)], True, (0,255,0),3, cv2.LINE_AA)
 
-        #Write to path
-        annotation_path = os.path.abspath(Path(RosPack().get_path("image_matching"))/"images")
+            #Write to path
+            annotation_path = os.path.abspath(Path(RosPack().get_path("image_matching"))/"images")
 
-        if not (os.path.exists(annotation_path)):
-            os.mkdir(annotation_path)
+            if not (os.path.exists(annotation_path)):
+                os.mkdir(annotation_path)
 
-        if not cv2.imwrite("{}/id_{}.jpg".format(annotation_path, img_msg.header.seq), img):
-            raise Exception("Could not write image")
+            if not cv2.imwrite("{}/id_{}.jpg".format(annotation_path, img_msg.header.seq), img):
+                raise Exception("Could not write image")
 
-        with open(os.path.join(annotation_path,"label.txt"), "a") as f:
-            temp_pts = np.reshape(dst,(4,2))
-            min_x, min_y = np.amin(temp_pts, axis=0)
-            w,h = abs(np.amax(temp_pts, axis=0) - (min_x, min_y))
-            f.write("id_{} {} {} {} {} {}\n".format(img_msg.header.seq, min_x, min_y, w, h, template))
-            f.close()
+            with open(os.path.join(annotation_path,"label.txt"), "a") as f:
+                temp_pts = np.reshape(dst,(4,2))
+                min_x, min_y = np.amin(temp_pts, axis=0)
+                w,h = abs(np.amax(temp_pts, axis=0) - (min_x, min_y))
+                f.write("id_{} {} {} {} {} {}\n".format(img_msg.header.seq, min_x, min_y, w, h, template))
+                f.close()
 
 if __name__ == "__main__":
     rospy.init_node("basic_bounding_box", anonymous=True, log_level=rospy.DEBUG)
