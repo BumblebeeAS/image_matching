6dd584fea89886ec38a18dabeea851bd345861a2
Seow Alex <seowalex@hotmail.com>
2023-05-12 19:34:02 +0800

    Concurrency fixes

--- scripts/pose_estimator_node.py
+++ scripts/pose_estimator_node.py
@@ -5,6 +5,7 @@ from operator import attrgetter
 import os
 from pathlib import Path
 import glob
+import copy
 
 import cv2
 import message_filters
@@ -25,7 +26,9 @@ from transforms3d.euler import quat2euler
 from feature_matcher.keypoints_match_producer import get_keypoints_match_producer
 from pose_estimator.PinholeCamera import PinholeCamera
 from pose_estimator.pose_estimator import PoseEstimator
+
 mutex = threading.Lock()
+
 
 class BasicPoseEstimator:
     def __init__(
@@ -57,9 +60,7 @@ class BasicPoseEstimator:
                 self.bridge.cv2_to_compressed_imgmsg(img, "jpeg")
             )
         )
-        self.pose_estimator.visualize_callbacks.append(
-            lambda img: print("visualising")
-        )
+        self.pose_estimator.visualize_callbacks.append(lambda img: print("visualising"))
         self.pose_estimator.register_template(
             template, template_dimensions, cv2.imread(template_path)
         )
@@ -90,20 +91,29 @@ class BasicPoseEstimator:
         #     1,
         # )
         # ts.registerCallback(self.cropped_image_callback)
-        rospy.Subscriber(input_topic, CompressedImage, self.msg_callback, queue_size=1, tcp_nodelay=True)
-        rospy.Timer(rospy.Duration(2), self.cropped_image_callback)
+        rospy.Subscriber(
+            input_topic,
+            CompressedImage,
+            self.msg_callback,
+            queue_size=1,
+            tcp_nodelay=True,
+        )
+        rospy.Timer(rospy.Duration(0.5), self.cropped_image_callback)
 
     def msg_callback(self, msg):
         mutex.acquire(blocking=True)
         self.latest_msg = msg
         mutex.release()
 
-
     def cropped_image_callback(self, debug=False):
-        if self.latest_msg is None:
-            return
         mutex.acquire(blocking=True)
-        img_msg = self.latest_msg
+        img_msg = (
+            copy.deepcopy(self.latest_msg) if self.latest_msg is not None else None
+        )
+        mutex.release()
+
+        if img_msg is None:
+            return
         detected_objects = None
         rospy.logdebug_throttle(10, f"Received image {img_msg.header.seq}")
         try:
@@ -137,26 +147,27 @@ class BasicPoseEstimator:
             lxtyrxby = None
 
         rot, trans = self.pose_estimator.compute_pose(
-            img, template, img_msg.header.frame_id, lxtyrxby=lxtyrxby, debug=True
+            img,
+            template,
+            img_msg.header.frame_id,
+            num_keypoints=100,
+            lxtyrxby=lxtyrxby,
+            debug=True,
         )
         if rot is not None and trans is not None and trans[2] > 0:
             self.publish_tf(
-                rot, trans, img_msg.header.frame_id, template, img_msg.header.stamp
+                rot, trans, img_msg.header.frame_id, "/auv4/" + template, img_msg.header.stamp
             )
-        mutex.release()
 
     def publish_tf(self, rot, trans, frame_id, child_frame_id, stamp):
         self.transform_stamped.transform.translation.x = trans[0]
         self.transform_stamped.transform.translation.y = trans[1]
         self.transform_stamped.transform.translation.z = trans[2]
-        
-
 
         _rot = np.eye(4)
         _rot[:3, :3] = rot
         quaternion = transformations.quaternion_from_matrix(_rot)
         if np.abs(quaternion.dot(quaternion) - 1) < 1e-6:
-
             pose_stamped = tf2_geometry_msgs.PoseStamped()
             pose_stamped.header.stamp = stamp
             pose_stamped.header.frame_id = frame_id
@@ -170,23 +181,46 @@ class BasicPoseEstimator:
 
             try:
                 new_pose_stamped = self.tf_buffer.transform(pose_stamped, "world_ned")
-                print(new_pose_stamped)
+                # print(new_pose_stamped)
             except Exception as e:
                 print(f"Could not transform {e}")
                 return
             self.transform_stamped.header = new_pose_stamped.header
-            self.transform_stamped.transform.translation.x = new_pose_stamped.pose.position.x
-            self.transform_stamped.transform.translation.y = new_pose_stamped.pose.position.y
-            self.transform_stamped.transform.translation.z = new_pose_stamped.pose.position.z
+            self.transform_stamped.transform.translation.x = (
+                new_pose_stamped.pose.position.x
+            )
+            self.transform_stamped.transform.translation.y = (
+                new_pose_stamped.pose.position.y
+            )
+            self.transform_stamped.transform.translation.z = (
+                new_pose_stamped.pose.position.z
+            )
 
-            self.transform_stamped.transform.rotation = new_pose_stamped.pose.orientation
+            self.transform_stamped.transform.rotation = (
+                new_pose_stamped.pose.orientation
+            )
 
             self.transform_stamped.header.stamp = new_pose_stamped.header.stamp
             self.transform_stamped.header.frame_id = new_pose_stamped.header.frame_id
             self.transform_stamped.child_frame_id = child_frame_id
 
-            roll, pitch, yaw = (np.array(quat2euler(attrgetter("w", "x", "y", "z")(self.transform_stamped.transform.rotation)))*180/np.pi) % 90
-            print(min(roll, 90-roll), min(pitch, 90-pitch), min(yaw, 90-yaw))
+            roll, pitch, yaw = (
+                np.array(
+                    quat2euler(
+                        attrgetter("w", "x", "y", "z")(
+                            self.transform_stamped.transform.rotation
+                        )
+                    )
+                )
+                * 180
+                / np.pi
+            ) % 90
+            print(
+                "RPY: ",
+                min(roll, 90 - roll),
+                min(pitch, 90 - pitch),
+                min(yaw, 90 - yaw),
+            )
             self.br.sendTransform(self.transform_stamped)
 
 
@@ -207,9 +241,10 @@ if __name__ == "__main__":
     visualization_topic = rospy.get_param("~visualization_topic", "/visualization")
     template = rospy.get_param("~template", "Bootlegger")
 
-
     # Accept either png or jpeg files
-    templates_dir = os.path.abspath(Path(RosPack().get_path("image_matching"))/"templates")
+    templates_dir = os.path.abspath(
+        Path(RosPack().get_path("image_matching")) / "templates"
+    )
     possible_templates = glob.glob(os.path.join(templates_dir, f"{template}.*"))
     if not possible_templates:
         rospy.logwarn_once(f"No template found for {template} in {templates_dir}")
@@ -275,12 +310,11 @@ if __name__ == "__main__":
     elif matcher == "orb_bf":
         image_match_producer = get_keypoints_match_producer(
             "orb", "bf", {"debug": True}, {"debug": True}
-    )
+        )
     elif matcher == "alike_bf":
         image_match_producer = get_keypoints_match_producer(
             "alike", "bf", {"debug": True}, {"debug": True}
         )
-
 
     pose_estimator = BasicPoseEstimator(
         camera_topic,
