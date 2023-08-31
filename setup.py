6970b73a0cab4549d6089e15f7b1309d4417715e
b3nguin <koh.benjamin@u.nus.edu>
2023-08-31 15:01:26 +0800

    Add docs submodule

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools',],
     zip_safe=True,
     maintainer='b3nguin',
     maintainer_email='koh.benjamin@u.nus.edu',
