1755f3b6a1c0785c63def5d4f63ea5bf73f2ef50
b3nguin <koh.benjamin@u.nus.edu>
2023-09-11 16:18:44 +0800

    Initial ROS2 Port

--- setup.py
+++ setup.py
@@ -2,10 +2,23 @@ from setuptools import setup
 
 package_name = 'image_matching'
 
+def generate_data_files(share_path, dir):
+    data_files = []
+
+    for path, _, files in os.walk(dir):
+        list_entry = (share_path + path, [os.path.join(path, f) for f in files if not f.startswith('.')])
+        data_files.append(list_entry)
+
+    return data_files
+
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=[
+        package_name,
+        "src/feature_matcher", 
+        "src/pose_estimator", 
+        "src/utils"],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -20,6 +33,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'detector = image_matching.detector:main',
         ],
     },
 )
